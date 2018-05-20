5386dd4d3c746fbafdc0dd496c8d51d149ac24b7
Spencer Baird <spencer.baird36@gmail.com>
2018-05-20 17:21:32 -0400

    initial landing page set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import basketballLogo from './basketball.svg';
+import baseballLogo from './baseball.svg';
+import footballLogo from './football.svg';
+import Plx from 'react-plx';
 
 class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
 
   render() {
+    const buttonStyle = {
+      backgroundColor: 'transparent',
+      color: 'white'
+    };
+
+    const baseballdata = [
+      {
+        start: 0,
+        end: 200,
+        easing: 'ease',
+        properties: [
+          {
+            startValue: 0,
+            endValue: 180,
+            property: "rotate"
+          }
+        ]
+      },
+    ];
+
     return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <AppBar
+            title="Fantasy Sports Ultimate"
+            iconElementRight={<FlatButton label="Sign In" style={buttonStyle} />}
+            iconElementLeft={<FlatButton label="Register" style={buttonStyle} />}
+            style={{position:"fixed"}}
+          />
+          <div style={{padding: "155px 0px 155px 0px"}}>
+            This is the ultimate fantasy sports site
+          </div>
+          <div>
+            <Card>
+              <Plx
+                parallaxData={baseballdata}
+              >
+                <img style={{marginLeft: '200px'}} src={baseballLogo} />
+
+              </Plx>
+              <Plx
+                parallaxData={baseballdata}
+              >
+                <img style={{marginLeft: '200px'}} src={basketballLogo} />
+              </Plx>
+              <Plx
+                parallaxData={baseballdata}
+              >
+                <img style={{marginLeft: '200px'}} src={footballLogo} />
+              </Plx>
+            </Card>
+
+          </div>
+          <div style={{padding: "155px 0px 155px 0px"}}></div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  combineReducers({
+  }),
+  applyMiddleware(thunk)
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path = '/' component={App} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
